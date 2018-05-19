aa1a1c4f55aa5afaa2cd6734d521a975da893b05
AqilSidek <aq493299@dal.ca>
2018-05-19 16:25:32 -0300

    Added the feature where button click will copy string in editText field onto the viewText field.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText field;
+    Button button;
+    TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //these three initalizers link variable in code to UI elements
+        field = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        text = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(field.getText());
+            }
+        });
+
     }
 }
